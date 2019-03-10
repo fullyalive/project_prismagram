3bfedb071d885cb67cfd53465143c7dc7389769d
fullyalive <wogud1202@gmail.com>
2019-03-10 16:59:36 +0900

    GraphQL 서버 구축
    yarn add @babel/node
    yarn add @babel/preset-env
    yarn remove babel-cli

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log("hello");
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.PORT || 7777;
+
+const typeDefs = `
+    type Query {
+        hello:String!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "hi"
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on port http://localhost:${PORT}`)
+);
