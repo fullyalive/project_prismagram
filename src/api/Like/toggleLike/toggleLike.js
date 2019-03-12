b7d03f229c3cd38bf6d81e2693af3620e508fe4a
fullyalive <wogud1202@gmail.com>
2019-03-13 02:52:03 +0900

    toggleLike | Like Resolver 생성

---
+++ src/api/Like/toggleLike/toggleLike.js
@@ -0,0 +1,48 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+      const filterOptions = {
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id: postId
+            }
+          }
+        ]
+      };
+      try {
+        const existingLike = await prisma.$exists.like(filterOptions);
+        if (existingLike) {
+          await prisma.deleteManyLikes(filterOptions); // deleteManyLikes : 내가 누른 라이크를 찾아서 삭제한다
+        } else {
+          await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id
+              }
+            },
+            post: {
+              connect: {
+                id: postId
+              }
+            }
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+};
