36e692c845dbd8d02be44144fd1d50e1565bf63a
fullyalive <wogud1202@gmail.com>
2019-05-12 21:25:17 +0900

    Me resolver

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,17 +4,28 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const exists = await prisma.$exists.user({
-        OR: [
-          {
-            username
-          },
-          { email }
-        ]
+      const userNameExists = await prisma.$exists.user({
+        username
       });
-      if (exists) {
+      const emailExists = await prisma.$exists.user({
+        email
+      });
+      if (userNameExists) {
         throw Error("이미 존재하는 닉네임입니다.");
+      } else if (emailExists) {
+        throw Error("이미 존재하는 이메일입니다.");
       }
+      //  const exists = await prisma.$exists.user({
+      //    OR: [
+      //      {
+      //        username
+      //      },
+      //      { email }
+      //    ]
+      //  });
+      //  if (exists) {
+      //    throw Error("This username / email is already taken");
+      //  }
       await prisma.createUser({
         username,
         email,
